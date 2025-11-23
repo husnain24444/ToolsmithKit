e4e39e7751bb43fdb32ac0c9848ac446e6ba0cfd
husnainali24444 <husnainali24444@gmail.com>
2025-11-24 01:40:44 +0500

    feat: add compression plugin and preload resources in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import compression from 'vite-plugin-compression';
 
 export default defineConfig({
   plugins: [
     react(),
+    compression(),
     runtimeErrorOverlay(),
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
